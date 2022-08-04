46176e32b7f68b5ecb4b5b7e3179e5a05ff2e40c
donovan <donovan@donovanwalker.com>
2022-08-04 15:18:21 -0500

    Remove useless cssNames tests.

--- src/css_names.test.js
+++ src/css_names.test.js
@@ -11,15 +11,4 @@ describe('cssNames', () => {
         const cName = cssNames('a', 'b')
         expect(cName.toString()).toBe('a b')
     })
-
-    it(`add() appends multiple strings`, () => {
-        const cName = cssNames('a', 'b')
-        cName.add('c', 'd', 'e')
-        expect(cName.toString()).toBe('a b c d e')
-    })
-    it(`push() still works`, () => {
-        const cName = cssNames('a', 'b')
-        cName.push('c', 'd', 'e')
-        expect(cName.toString()).toBe('a b c d e')
-    })
 })
